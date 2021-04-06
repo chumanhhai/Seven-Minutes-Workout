59a57ca442f78fd6b83470aa9ca397cb4491de69
manh hai <chumanhhailtt@gmail.com>
2021-04-06 09:18:17 +0700

    fix countdown not responding

--- app/src/main/java/chumanhhailtt/sevenminutesworkout/ExerciseActivity.kt
+++ app/src/main/java/chumanhhailtt/sevenminutesworkout/ExerciseActivity.kt
@@ -17,9 +17,10 @@ class ExerciseActivity : AppCompatActivity() {
     var llGetReady: LinearLayout? = null
     var llDoingExercise: LinearLayout? = null
     var counter: CountDownTimer? = null
+    var tvExerciseName : TextView? = null
 
     var rest = true
-    var exerciseLevel = -1
+    var exerciseLevel = 0
 
     val COUNTDOWN_READY_NUM = 10
     val COUNTDOWN_READY_TITLE = "GET READY FOR"
@@ -38,12 +39,10 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         // get view
-        pbCountdown = findViewById(R.id.pb_countdown)
-        tvCountdownNumber = findViewById(R.id.tv_countdown_number)
-        tvCountdownTitle = findViewById(R.id.tv_countdown_title)
         ivExercise = findViewById(R.id.iv_exercise)
         llGetReady = findViewById(R.id.ll_get_ready)
         llDoingExercise = findViewById(R.id.ll_doing_exercise)
+        tvExerciseName = findViewById(R.id.tv_exercise_name)
 
         // setup
         executeRest()
@@ -62,9 +61,9 @@ class ExerciseActivity : AppCompatActivity() {
             }
             override fun onFinish() {
                 if(rest) {
-                    if(exerciseLevel < Constants.exercises.size-1) {
+                    exerciseLevel++
+                    if(exerciseLevel < Constants.exercises.size) {
                         rest = false
-                        exerciseLevel++
                         executeDoExcercise()
                     }
                 } else {
@@ -80,9 +79,15 @@ class ExerciseActivity : AppCompatActivity() {
         llGetReady!!.visibility = View.GONE
         llDoingExercise!!.visibility = View.VISIBLE
 
+        // set view
+        pbCountdown = llDoingExercise!!.findViewById(R.id.pb_countdown)
+        tvCountdownNumber = llDoingExercise!!.findViewById(R.id.tv_countdown_number)
+        tvCountdownTitle = llDoingExercise!!.findViewById(R.id.tv_countdown_title)
+
         val exercise = Constants.exercises.get(exerciseLevel)
         ivExercise!!.setImageResource(exercise.image)
         tvCountdownTitle!!.text = COUNTDOWN_DOING_EXCERCISE_TITLE
+        tvExerciseName!!.text = exercise.name
         countdown(exercise.length)
     }
 
@@ -90,6 +95,11 @@ class ExerciseActivity : AppCompatActivity() {
         // set visibility
         llGetReady!!.visibility = View.VISIBLE
         llDoingExercise!!.visibility = View.GONE
+
+        // set view
+        pbCountdown = llGetReady!!.findViewById(R.id.pb_countdown)
+        tvCountdownNumber = llGetReady!!.findViewById(R.id.tv_countdown_number)
+        tvCountdownTitle = llGetReady!!.findViewById(R.id.tv_countdown_title)
 
         tvCountdownTitle!!.text = COUNTDOWN_READY_TITLE
         countdown(COUNTDOWN_READY_NUM)
