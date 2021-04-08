66c29ec3e36d5c8ab2ffab938852b82f52f6fb9b
manh hai <chumanhhailtt@gmail.com>
2021-04-08 08:42:12 +0700

    add raido group feature

--- app/src/main/java/chumanhhailtt/sevenminutesworkout/IBMActivity.kt
+++ app/src/main/java/chumanhhailtt/sevenminutesworkout/IBMActivity.kt
@@ -2,6 +2,7 @@ package chumanhhailtt.sevenminutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.widget.Toolbar
@@ -11,12 +12,19 @@ import java.lang.Exception
 class IBMActivity : AppCompatActivity() {
 
     var toolbarIBMActivity: Toolbar? = null
-    var etWeight: EditText? = null
-    var etHeight: EditText? = null
+    var etMetricWeight: EditText? = null
+    var etMetricHeight: EditText? = null
+    var etUsWeight: EditText? = null
+    var etUsHeight: EditText? = null
     var tvIBMScore: TextView? = null
     var tvIBMType: TextView? = null
     var btnIBMCaculate: Button? = null
-    var llIBMInput: LinearLayout? = null
+    var llIBMResult: LinearLayout? = null
+    var llMetricUnit: LinearLayout? = null
+    var llUsUnit: LinearLayout? = null
+    var rgUnit: RadioGroup? = null
+
+    var metricUnit = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,42 +39,80 @@ class IBMActivity : AppCompatActivity() {
         }
 
         // get view
-        etWeight = findViewById(R.id.et_weight)
-        etHeight = findViewById(R.id.et_height)
+        etMetricWeight = findViewById(R.id.et_metric_weight)
+        etMetricHeight = findViewById(R.id.et_metric_height)
+        etUsWeight = findViewById(R.id.et_us_weight)
+        etUsHeight = findViewById(R.id.et_us_height)
         tvIBMScore = findViewById(R.id.tv_ibm_score)
         tvIBMType = findViewById(R.id.tv_ibm_type)
         btnIBMCaculate = findViewById(R.id.btn_ibm_calculate)
-        llIBMInput = findViewById(R.id.ll_ibm_input)
+        llIBMResult = findViewById(R.id.ll_ibm_result)
+        llMetricUnit = findViewById(R.id.llMetricUnit)
+        llUsUnit = findViewById(R.id.llUsUnit)
+        rgUnit = findViewById(R.id.rg_unit)
 
         btnIBMCaculate!!.setOnClickListener {
-            if(etHeight!!.text.isNullOrEmpty() || etWeight!!.text.isNullOrEmpty()) {
-                Toast.makeText(this, "Please enter full information!", Toast.LENGTH_SHORT).show()
-            } else {
-                val height: Float = etHeight!!.text.toString().toFloat() / 100
-                val weight: Float = etWeight!!.text.toString().toFloat()
-                val score = weight / (height*height)
-                tvIBMScore!!.text = score.toString()
-                if(score < 16) {
-                    tvIBMType!!.text = "Severe Thinness"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                } else if(score < 17) {
-                    tvIBMType!!.text = "Moderate Thinness"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                } else if(score < 18.5) {
-                    tvIBMType!!.text = "Mild Thinness"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
-                } else if(score < 25) {
-                    tvIBMType!!.text = "Normal"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.green))
-                } else if(score < 30) {
-                    tvIBMType!!.text = "Overweight"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.red))
+            if(metricUnit) {
+                if(etMetricWeight!!.text.isNullOrBlank() || etMetricHeight!!.text.isNullOrBlank()) {
+                    Toast.makeText(this, "Please enter full information!", Toast.LENGTH_SHORT).show()
                 } else {
-                    tvIBMType!!.text = "Obese (Dit me beo vai lon)"
-                    tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    val height: Float = etMetricHeight!!.text.toString().toFloat() / 100
+                    val weight: Float = etMetricWeight!!.text.toString().toFloat()
+                    val score = 703*weight / (height*height)
+                    setIBMResult(score)
                 }
-                llIBMInput!!.visibility = View.VISIBLE
+            } else {
+                if(etUsWeight!!.text.isNullOrBlank() || etUsHeight!!.text.isNullOrBlank()) {
+                    Toast.makeText(this, "Please enter full information!", Toast.LENGTH_SHORT).show()
+                } else {
+                    val height: Float = etUsHeight!!.text.toString().toFloat() / 100
+                    val weight: Float = etUsWeight!!.text.toString().toFloat()
+                    val score = weight / (height*height)
+                    setIBMResult(score)
+                }
             }
         }
+
+        rgUnit!!.setOnCheckedChangeListener { group, checkedId ->
+            if(checkedId == R.id.rb_metric) {
+                llMetricUnit!!.visibility = View.VISIBLE
+                llUsUnit!!.visibility = View.GONE
+                llIBMResult!!.visibility = View.INVISIBLE
+                etMetricHeight!!.text.clear()
+                etMetricWeight!!.text.clear()
+                metricUnit = true
+            } else if(checkedId == R.id.rb_us){
+                llUsUnit!!.visibility = View.VISIBLE
+                llMetricUnit!!.visibility = View.GONE
+                llIBMResult!!.visibility = View.INVISIBLE
+                etUsWeight!!.text.clear()
+                etUsHeight!!.text.clear()
+                metricUnit = false
+            }
+        }
+    }
+
+    private fun setIBMResult(score: Float) {
+        tvIBMScore!!.text = score.toString()
+        if(score < 16) {
+            tvIBMType!!.text = "Severe Thinness"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
+        } else if(score < 17) {
+            tvIBMType!!.text = "Moderate Thinness"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
+        } else if(score < 18.5) {
+            tvIBMType!!.text = "Mild Thinness"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.yellow))
+        } else if(score < 25) {
+            tvIBMType!!.text = "Normal"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.green))
+        } else if(score < 30) {
+            tvIBMType!!.text = "Overweight"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.red))
+        } else {
+            tvIBMType!!.text = "Obese (Dit me beo vai lon)"
+            tvIBMType!!.setTextColor(ContextCompat.getColor(this, R.color.red))
+        }
+        llIBMResult!!.visibility = View.VISIBLE
     }
 }
